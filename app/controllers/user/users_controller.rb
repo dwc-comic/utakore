12e05f04479646936e918f87d333056d5f2870b9
shizukaota <anewdayhascomekorukoru@gmail.com>
2019-04-18 10:06:24 +0000

    デバイスのuserとadminを追加しました

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,7 +1,18 @@
 class User::UsersController < User::Base
   def show
+  	@user = User.find(params[:id])
   end
 
   def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	user = user.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+  def user_params
+        params.require(:user).permit([:name, :email, :kana, :phone_number, :postal_code, :home_address])
   end
 end
