f416825c7ace4188aaf7cd1d4220b48359c286a5
kentakasama <kasaken1113@gmail.com>
2019-04-25 09:53:33 +0000

    Orderコントローラー作ってそれらに全て、New、Create、Indexを追加いたしました

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -1,10 +1,40 @@
 class User::OrdersController < User::Base
 
+def new
+  	p current_user
+  	@user = current_user
+    @cart = Cart.find_by(user_id: @user.id)
+    # @cart_items = CartItem.where(cart_id: @cart)
+    order = Orders.new
+    order_item = OrderItems.new
+  end
+
+  def create
+  	carts = current_user.carts.where(deleted_at: nil)
+  	order = Orders.new
+  	order.user_id = current_user.id
+  	order.user_id = current_user.postal_code
+  	order.user_id = current_user.home_address
+  	#下にカートIDを保存するアクションを書く
+  	order.cart_id = params[:order][:cart_id]
+  	order.save
+
+  	#下記はorder_itemの保存
+  	carts.each do |cart|
+  		order_item.order_id = order.id
+  		order_item.item_id = params[:order_item][:item_id]
+  		order_item.ordered_price = params[:order_item][:item_price]
+  		order_item.ordered_quantity = params[:order_item][:cart_quantity]
+  		order_item.save
+  	end
+  	#order_itemの保存のアクションをOrderテーブル保存後にやる。
+  	redirect_to user_orders_path
+  end
+
+
   def index
   end
 
   def show
   end
 end
-
-
--- app/controllers/user/purchaces_controller.rb
+++ app/controllers/user/purchaces_controller.rb
@@ -6,16 +6,20 @@ class User::PurchacesController < User::Base
     # @cart_items = CartItem.where(cart_id: @cart)
   end
 
-  def buy
+  def create
   	carts = current_user.carts.where(deleted_at: nil)
-  	order = Order.new
-  	#binding.pay
+  	order = Orders.new
   	carts.each do |cart|
-  		cart.destroy
   		order.cart_id = cart.id
   		order.user_id = current_user.id
+  		#合計金額をOrderに追加したい
+  		#order.total_price = cart.total_price(cart)
+  		order.name = current_user.name
+  		order.postal_code = current_user.postal_code
+  		order.home_address = current_user.home_address
   		order.save
   	end
-  	redirect_to purchace_show_path
+  	redirect_to user_purchaces_show_path
   end
 end
+
