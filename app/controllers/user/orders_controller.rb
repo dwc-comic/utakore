c20e332194865f215262a0e7559c38199ecd5e2a
kentakasama <kasaken1113@gmail.com>
2019-04-25 07:49:45 +0000

    購入画面を作成いたしました。

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -1,9 +1,6 @@
 class User::OrdersController < User::Base
 
   def index
-  	@user = current_user
-    @cart = Cart.find_by(user_id: @user.id)
-    # @cart_items = CartItem.where(cart_id: @cart)
   end
 
   def show
--- app/controllers/user/purchaces_controller.rb
+++ app/controllers/user/purchaces_controller.rb
@@ -1,7 +1,21 @@
 class User::PurchacesController < User::Base
   def index
+  	p current_user
+  	@user = current_user
+    @cart = Cart.find_by(user_id: @user.id)
+    # @cart_items = CartItem.where(cart_id: @cart)
   end
 
-  def pay
+  def buy
+  	carts = current_user.carts.where(deleted_at: nil)
+  	order = Order.new
+  	#binding.pay
+  	carts.each do |cart|
+  		cart.destroy
+  		order.cart_id = cart.id
+  		order.user_id = current_user.id
+  		order.save
+  	end
+  	redirect_to purchace_show_path
   end
 end
