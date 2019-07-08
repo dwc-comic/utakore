bd48fad7b1fdd511b6f5e09fd85d39780ace5d7e
shizukaota <anewdayhascomekorukoru@gmail.com>
2019-07-08 14:27:00 +0000

    cart,cart-item d

--- app/models/cart_item.rb
+++
@@ -1,8 +0,0 @@
-class CartItem < ApplicationRecord
-	belongs_to :cart
-	belongs_to :item
-
-	def total_price
-		item.item_price * cart_quantity
-	end
-end
---
+++ db/migrate/20190708141855_d.rb
@@ -0,0 +1,4 @@
+class D < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
